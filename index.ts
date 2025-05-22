dcc05012dcc1717ec94b02be791427aab0913be4
Divyansh chaudhary <hypr123890@gmail.com>
2025-05-22 17:42:27 +0530

    Add additional TypeScript variable declarations and object types in index.ts

--- index.ts
+++ index.ts
@@ -16,3 +16,17 @@ console.log(bool);
 const arr: number[] = [1,2,3,4];
 const str: string[] = ['sheetal','divyansh','manish']
 
+const obj: object = {
+    name: "Ram",
+    age: 20,
+    class: 12
+}
+
+const obj1: {name:string, age:number} = {
+    name: "Bhola",
+    age: 38
+}
+
+
+const customArr : [number, string] = [40,"Bhola Chaudhary"]
+
