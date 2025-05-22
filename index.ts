bca8bbb9e2d33e507eaed592e41e83eb1b4d1a77
Divyansh chaudhary <hypr123890@gmail.com>
2025-05-22 17:40:01 +0530

    Enhance index.ts with additional TypeScript variable declarations and console outputs

--- index.ts
+++ index.ts
@@ -2,3 +2,17 @@
 let a = 10;
 a = 10;
 console.log(a); 
+
+
+const s:string = "Bhola Chaudhary"
+console.log(s);
+
+
+let bool:boolean = false;
+bool = true
+console.log(bool);
+
+
+const arr: number[] = [1,2,3,4];
+const str: string[] = ['sheetal','divyansh','manish']
+
