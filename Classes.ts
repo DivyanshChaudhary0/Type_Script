75fa742fa1ea242f186f6cb8764385af9e3acba0
Divyansh chaudhary <hypr123890@gmail.com>
2025-05-25 12:19:59 +0530

    Refactor Classes.ts to include Car5 class with access modifiers and instance creation

--- Classes.ts
+++ Classes.ts
@@ -15,3 +15,29 @@ class Person5 {
 
 const new_Person = new Person5('Bhola', 20);
 console.log(new_Person.greet());
+
+
+// Access Modifiers
+// public - access from anywhere, this is default.
+// private - access only within the class.
+// protected - access within the class and its subclasses
+class Car5{
+    public name: string;
+    private model: string;
+    protected price: number;
+
+    constructor(name: string, model: string, price: number){
+        this.name = name;
+        this.model = model;
+        this.price = price;
+    }
+
+    public getModel(): string{
+        return `Model is ${this.model}`
+    }
+}
+
+const swift = new Car5('Swift','Z15442',200000);  // we can only access getModel and name outside the class
+console.log(swift.getModel());
+console.log(swift.name);
+
