07160b0d0a077741681379995bf91db381ce427b
Divyansh chaudhary <hypr123890@gmail.com>
2025-05-27 18:37:42 +0530

    Add Employee and Manager classes with inheritance and details method

--- practice_problems_set2.ts
+++ practice_problems_set2.ts
@@ -29,3 +29,50 @@ myAccount.deposit(1200);
 console.log(myAccount.getBalance());
 myAccount.withdraw(1200);
 console.log(myAccount.getBalance());
+
+
+/*
+
+2.  Create an employee class with inheritance:
+    Define an employee class with property name and salary,
+    Create a subclass Manager that adds a departement property.
+    Both classes should have a getDeatils method that returns a string with the employee's details.
+
+*/
+
+class Employee{
+    name: string;
+    salary: number;
+
+    constructor(name: string, salary: number){
+        this.name = name;
+        this.salary = salary;
+    }
+
+    getDetails(): string{
+        return `Name of the employee is ${this.name} and salary is ${this.salary}`
+    }
+
+}
+
+
+class Manager extends Employee{
+
+    department: string;
+
+    constructor(name: string, salary: number, department: string){
+        super(name,salary);
+        this.department = department;
+    }
+
+    getDetails(): string {
+        return `Name of the manager is ${this.name} and salary is ${this.salary}`
+    }
+}
+
+const manish = new Employee('Manish',5000);
+console.log(manish.getDetails());
+
+const manager = new Manager('Sheetal',40000,'IT');
+console.log(manager.getDetails());
+
