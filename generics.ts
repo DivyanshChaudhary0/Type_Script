f16b01029f0babc71b21abc6c865bf047dc8988d
Divyansh chaudhary <hypr123890@gmail.com>
2025-05-30 21:56:43 +0530

    Add getFirstElement function to demonstrate generics with arrays

--- generics.ts
+++ generics.ts
@@ -10,3 +10,10 @@ function identity<T>(arg: T): T {
 
 let output1 = identity<string>("hello"); // Explicit
 let output2 = identity(100);             // Inferred
+
+
+function getFirstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+const first = getFirstElement<number>([1, 2, 3]);
