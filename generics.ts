dceb2864f06452559d26f973d0118f8bdce2a6f3
Divyansh chaudhary <hypr123890@gmail.com>
2025-05-30 21:55:11 +0530

    Add identity function with generic type and example usages

--- generics.ts
+++ generics.ts
@@ -4,3 +4,9 @@
                 different types without losing type safety.
 */
 
+function identity<T>(arg: T): T {
+  return arg;
+}
+
+let output1 = identity<string>("hello"); // Explicit
+let output2 = identity(100);             // Inferred
