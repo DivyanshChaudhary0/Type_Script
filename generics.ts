fb50e897eb16ffe595115ff47a498c0e44985a6d
Divyansh chaudhary <hypr123890@gmail.com>
2025-05-31 20:08:10 +0530

    Add getLength function to demonstrate generic constraints in generics.ts

--- generics.ts
+++ generics.ts
@@ -30,3 +30,11 @@ let keyPairValue: keyPair<string,number> = {
   value: 10
 }
 
+// generic constraints
+function getLength<T extends {length: number}>(item:T):number {
+  return item.length
+}
+
+console.log(getLength('Hello'));
+console.log(getLength([1,2,3,4,5]));
+
